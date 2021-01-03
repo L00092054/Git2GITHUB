02586751d74ff2a1e394a9df94cc7e8ce4d0c0ed
Damien Duffy <57752037+L00092054@users.noreply.github.com>
2021-01-03 00:04:22 +0000

    Update BubbleSorttest.java

--- BubbleSorttest.java
+++ BubbleSorttest.java
@@ -1,59 +1,31 @@
 public class BubbleSorttest {
-   private int swaps = 0;
-   private int comp = 0;
-   private long endTime ;
-   public void bubbleSort(int[] arr) {
-    //int bSwaps = 0;
-    //int comp = 0;
-    //long endTime;
-      StopWatch clock = new StopWatch();
-      clock.start();
+   static void bubbleSort(int[] arr) {
       int n = arr.length;
-   
-
       int temp = 0;
-      for (int i = 0; i < n-1; i++)  // Looping through the array length
-      {
-       
-         for (int j = 0; j < n-i-1; j++)
-         {
-            comp++;
-            
-         
-            if (arr[j] > arr[j+1]) 
-            {
-               // swap elements
-               temp = arr[j]; 
-               arr[j] = arr[j+1]; 
-               arr[j+1] = temp; 
-               
-               swaps+=1;
-              
-             
+
+      for(int i = 0; i < n; i++) {
+         for(int j=1; j < (n-i); j++) {
+            if(arr[j-1] > arr[j]) {
+               temp = arr[j-1];
+               arr[j-1] = arr[j];
+               arr[j] = temp;
             }
-         
          }
       }
-      clock.stop();
-      endTime =clock.getElapsedTime();
-     
-   
-      System.out.println("\n"+"Timer"+":"+clock.getElapsedTime());
-     
-   
-   	 
    }
+   public static void main(String[] args) {
+      int arr[] = { 2, 5, -2, 6, -3, 8, 0, -7, -9, 4 };
+      System.out.println("Array Before Bubble Sort");
 
-   
-   public int getBubbleSwaps(){
-      return swaps;
-   } 
-   public int getBubbleComp(){
-      return comp;
-   } 
-   public long getTime(){
-      return endTime;
-   } 
+      for(int i = 0; i < arr.length; i++) {
+         System.out.print(arr[i] + " ");
+      }
+      System.out.println();
+      bubbleSort(arr);
+      System.out.println("Array After Bubble Sort");
 
-	 
+      for(int i = 0; i < arr.length; i++) {
+         System.out.print(arr[i] + " ");
+      }
+   }
 }
